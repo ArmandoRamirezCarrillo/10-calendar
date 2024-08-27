d63ebdda385b316a85955ae6d3f284b00307246a
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2024-08-27 14:20:43 -0600

    Proyecto calendario

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage } from "../auth"
+import { CalendarPage } from "../calendar";
+
+const routerLogin = {
+    path: '/auth/*',
+    element: <LoginPage/>,
+};
+
+const routerCalendar = {
+    path: '/',
+    element: <CalendarPage/>,
+}
+
+export const AppRouter = () => {
+    const authStatus = 'authenticated';
+
+    const router = createBrowserRouter([
+        authStatus === 'not-authenticated' ? routerLogin : routerCalendar
+    ]);
+
+    return (
+    <>
+        <RouterProvider router={router}/>
+    </>
+    )
+}
