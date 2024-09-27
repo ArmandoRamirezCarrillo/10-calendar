30e556ef7b7cae63ceeb57d82a9aa7395f067b24
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2024-09-26 18:28:16 -0600

    Creando Slice para el auth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,28 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { authRoutes, AuthRoutes } from '../auth/routes/AuthRoutes';
 import { calendarRouter, CalendarRouter } from '../calendar/routes/CalendarRouter';
 
-const AppRouters = [
+// const AppRouters = [
+//     {
+//         path: '/auth',
+//         element: <AuthRoutes/>,
+//         children: authRoutes
+//     },
+//     {
+//         path: '/',
+//         element: <CalendarRouter/>,
+//         children: calendarRouter
+//     }
+// ]
+
+const AppRouterAuth = [
     {
         path: '/auth',
         element: <AuthRoutes/>,
         children: authRoutes
     },
+]
+
+const AppRouterCalendar = [
     {
         path: '/',
         element: <CalendarRouter/>,
@@ -16,13 +32,13 @@ const AppRouters = [
 ]
 
 export const AppRouter = () => {
-    // const authStatus = 'not-authenticated';
+    const authStatus = 'not-authenticated';
 
-    // const router = createBrowserRouter([
-    //     authStatus === 'not-authenticated' ? routersLogin : routersCalendar
-    // ]);
+    const router = createBrowserRouter([
+        authStatus === 'not-authenticated' ? AppRouterAuth : AppRouterCalendar
+    ]);
 
-    const router = createBrowserRouter(AppRouters);
+    // const router = createBrowserRouter(AppRouters);
 
     return (
     <>
